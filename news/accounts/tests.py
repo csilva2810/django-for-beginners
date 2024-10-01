84b1f30a4a90ca729dbb18468bed28120d5da2c3
Carlos Silva <csilva2810@gmail.com>
2024-09-30 21:13:34 -0300

    Implement user registration

--- news/accounts/tests.py
+++ news/accounts/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
 
 class UserManagersTests(TestCase):
@@ -28,3 +29,33 @@ class UserManagersTests(TestCase):
         self.assertTrue(user.is_active)
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
+
+
+class TestSignUp(TestCase):
+    def test_with_correct_template(self):
+        response = self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/signup.html")
+
+    def test_signup_user(self):
+        User = get_user_model()
+        user = {
+            "username": "testuser",
+            "email": "testuser@gmail.com",
+            "age": 33,
+            "password1": "testdjango123",
+            "password2": "testdjango123",
+        }
+        response = self.client.post(
+            reverse("signup"),
+            user,
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("login"))
+
+        db_user = User.objects.get(username=user["username"])
+        self.assertEqual(db_user.email, user["email"])
+        self.assertEqual(db_user.age, user["age"])
+        self.assertEqual(db_user.is_staff, False)
+        self.assertEqual(db_user.is_superuser, False)
